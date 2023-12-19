5349ba96c20406dd35aef97ee8057c453173e542
José Magalhães <joses.magalhaes@hotmail.com>
2023-12-19 13:10:34 -0300

    :hammer: Definição de variáveis iniciais

---
+++ giftrun.js
@@ -0,0 +1,35 @@
+SKY_COLOR = '#b4b4ff';  // Cor do céu
+NUM_SNOWFLAKES = 10;  // Número de flocos de neve
+
+image_names = ['reindeer1',
+               'reindeer2',
+               'rudolph1',
+               'rudolph2',
+               'santa1',
+               'santa2',
+               'santa3',
+               'chimney',
+               'gift',
+               'splat',
+               'star',
+               'smoke1',
+               'smoke2',
+               'poof',
+               'snowflake'];
+
+sound_names = ['plingeling',
+               'poof',
+               'whistle',
+               'splat'];
+
+images = {};
+sounds = {};
+
+move_left = false;
+move_right = false;
+move_up = false;
+move_down = false;
+move_drop = false;
+
+play_sounds = true;
+play_music = false;
