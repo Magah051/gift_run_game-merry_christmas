f314d67a634be3a9038a1d634b196ee75a5daf9f
José Magalhães <joses.magalhaes@hotmail.com>
2023-12-19 17:58:43 -0300

    :hammer: Implementação de movimentação básica

--- giftrun.js
+++ giftrun.js
@@ -53,3 +53,50 @@ function Vec2(x, y) {
 function vec(x, y) {
     return new Vec2(x, y);
 }
+
+// Chama a função para cada elemento no array
+// e remove-o se a função retornar verdadeiro.
+// A lista é iterada na ordem inversa.
+function removeif(arr, func) {
+    for(var i = arr.length - 1; i >= 0; i--) {
+        if(func(arr[i])) {
+            arr.splice(i, 1);
+        }
+    }
+}
+
+// Chama a função para cada item na lista
+function iter(arr, func) {
+    for(var i = 0; i < arr.length; i++) {
+        func(arr[i]);
+    }
+}
+
+function randint(high) {
+    return Math.floor(Math.random() * high);
+}
+
+function randrange(low, high) {
+    return low + Math.floor(Math.random() * high);
+}
+
+function Sprite(pos, motion, image) {
+    this.pos = pos;
+    this.motion = motion;
+    this.image = image;
+
+    this.move = function(speed) {
+        this.pos.x += speed;
+        this.pos.add(this.motion);
+    };
+
+    this.draw = function() {
+        ctx.drawImage(images[this.image], this.pos.x, this.pos.y);
+    };
+}
+
+function House(pos, motion, image) {
+    this.superClass = Sprite;
+    this.superClass(pos, motion, image);
+    this.smoking = (randint(4) == 0);  // 1 em 4 casas solta fumaça
+}
